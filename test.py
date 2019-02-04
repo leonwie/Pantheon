616df440d33227d3850cc5ecd33448e91090021d
leonwie <leonwie@users.noreply.github.com>
2019-02-04 20:03:39 +0000

    working on it

--- test.py
+++ test.py
@@ -12,12 +12,21 @@ bus.write_i2c_block_data(DEVICE_ADDRESS, CONFIG_POINTER, CONFIG_DATA) #write the
 
 
 
-while 1:
+#while 1:
+#    data = bus.read_i2c_block_data(DEVICE_ADDRESS, CONV_POINTER, 2)
+#    value = data[0] * 256 + data[1]
+#    if value > 32767:
+#	       value -= 65535
+#    voltage = 0.000125*value + 0.62 # scaling factor to convert to actual voltage
+#    #ms = -(25/3111111111)*math.sqrt(24888888888000000*voltage-11462666687111111) - 783333325/3111111111
+#    print(voltage)
+#    time.sleep(0.05)
+
+def read_data:
     data = bus.read_i2c_block_data(DEVICE_ADDRESS, CONV_POINTER, 2)
     value = data[0] * 256 + data[1]
     if value > 32767:
 	       value -= 65535
     voltage = 0.000125*value + 0.62 # scaling factor to convert to actual voltage
     #ms = -(25/3111111111)*math.sqrt(24888888888000000*voltage-11462666687111111) - 783333325/3111111111
-    print(voltage)
-    time.sleep(0.05)
+    return voltage
