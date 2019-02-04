cbfda9acee4cea1db1e69f648fbafe0a0d27a189
leonwie <leonwie@users.noreply.github.com>
2019-02-04 15:02:02 +0000

    second commit

---
+++ mqtt_mosquitto.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+import time
+def on_message(client, userdata, message) {
+    print("Received message: {} on topic {}" .format(time.clock_gettime(),time.clock_gettime()))
+}
+
+client = mqtt.Client()
+client.connect("test.mosquitto.org",port=1883)
+client.publish("IC.embedded/Pantheon/test","Connected to raspberry")
+
+mqtt.error_string(RETURN_CODE)
+
+client.on_message = on_message
+
+client.subscribe("IC.embedded/Pantheon/#")
+
+while True:
+    clent.loop()
+    time.sleep(1)
