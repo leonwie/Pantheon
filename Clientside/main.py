5f3816d1cf3c98364b7eec773db66dc37066d62a
leonwie <leonwie@users.noreply.github.com>
2019-02-12 13:46:31 +0000

    retry connection until succesfull

--- Clientside/main.py
+++ Clientside/main.py
@@ -1,7 +1,6 @@
 import paho.mqtt.client as mqtt
 import time
 import pyrebase
-import time
 
 config = {
     "apiKey": "AIzaSyCAnkKBia6Jd8REycaDryC2AY5Jj_NQBpQ",
@@ -15,56 +14,64 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
+#If connected to mqtt
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("IC.embedded/Pantheon/#")
+    client.publish("IC.embedded/Pantheon/test","Connected to computer2")
+
 #If a message is received
 def on_message(client, userdata, message):
-	print("Received message on computer ",message.payload)
-	print("Typeof",type(message.payload))
-	print("Message topic: ",message.topic)
+    print("Received message on computer ",message.payload)
+    print("Typeof",type(message.payload))
+    print("Message topic: ",message.topic)
 	#Check for message topic
-	if(message.topic=="IC.embedded/Pantheon/Measurement"):
-		send=str(message.payload)
+    if(message.topic=="IC.embedded/Pantheon/Measurement"):
+        send=str(message.payload)
 		#create dataobject
-		data1 = {
-	    	"Time:":time.ctime(),
-	    	"Downforce":send[2:len(send)-1]
-			}
-		data2 = {
-	    	"Downforce":send[2:len(send)-1]
-			}
-		print(data1)
+        data1 = {
+        "Time:":time.ctime(),
+        "Downforce":send[2:len(send)-1]
+		}
+        data2 = {
+        "Downforce":send[2:len(send)-1]
+        }
+        print(data1)
 		#send the data to the cloud
-		send_to_cloud(data1)
-		update_cloud(data2)
+        send_to_cloud(data1)
+        update_cloud(data2)
 
 #initialize the client
 client = mqtt.Client()
-
-#client.tls_set(ca_certs="mosquitto.org.crt", certfile="client.crt",keyfile="client.key")
+client.on_connect = on_connect
 client.connect("test.mosquitto.org",port=1883)
-
 client.publish("IC.embedded/Pantheon/test","Connected to computer")
-
-#mqtt.error_string(MSG_INFO.rc) #MSG_INFO is result of publish()
+#print(hello)
 
 client.on_message = on_message
 
-client.subscribe("IC.embedded/Pantheon/#")
+#client.subscribe("IC.embedded/Pantheon/#")
 
-def stream_handler(message):
-	if(message[data]=='On'):
-		client.publish("IC.embedded/Pantheon/run","On")
-	if(message[data]=='Off'):
-		client.publish("IC.embedded/Pantheon/run","Off")
+#def stream_handler(message):
+#    if(message["data"]=='On'):
+#        client.publish("IC.embedded/Pantheon/run","On")
+#        print("On")
+#    if(message["data"]=='Off'):
+#        client.publish("IC.embedded/Pantheon/run","Off")
+#        print("Off")
 
-my_stream = db.child("Reading").stream(stream_handler)
+#my_stream = db.child("Reading").stream(stream_handler)
 
 def send_to_cloud(data):
     results = db.child("Downforces").push(data)
 def update_cloud(data):
     results = db.child("Downforce").update(data)
-
+client.loop_forever()
 
 #endless loop
-while True:
-	client.loop()
-	time.sleep(2)
+#while True:
+#    client.loop()
+#    time.sleep(2)
