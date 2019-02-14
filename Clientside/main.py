0b3f71b369368f8a5c96c03d6acb6ed6f6d819d8
leonwie <leonwie@users.noreply.github.com>
2019-02-14 12:32:26 +0000

    Adding submodule

--- Clientside/main.py
+++ Clientside/main.py
@@ -3,11 +3,12 @@ import time
 import pyrebase
 import sys
 
-#wing info
+#Wing Info
 WINGSPAN = 10
 CHORD = 10
 LIFTCOEFFICIENT = 10
 
+#Configure Firebase database
 config = {
     "apiKey": "AIzaSyCAnkKBia6Jd8REycaDryC2AY5Jj_NQBpQ",
     "authDomain": "formula-1-7a0c8.firebaseapp.com",
@@ -17,6 +18,7 @@ config = {
     "messagingSenderId": "708407712539"
 }
 
+#Initialize firebase
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
@@ -40,28 +42,38 @@ def on_message(client, userdata, message):
 	#Check for message topic
     if(message.topic=="IC.embedded/Pantheon/Measurement/concatData"):
         print("Measurement received")
+        #Receive the sensordata
         sensordata=message.payload
-        sensordata_dec=sensordata.decode("utf-8")
+        #Decode the sensordata
+        sensordata=sensordata.decode("utf-8")
+        #Split the sensordata up into the individual measurements
         sensorvalues=sensordata_dec.split(",")
+        #Split the array into different variables and convert to float
         temperature = float(sensorvalues[0])
         airpressure = float(sensorvalues[1])
         airflow = float(sensorvalues[2])
-    #print("Sensordata: ")
+    #Convert temperature to kelvin
     tempk = temperature + 273.15
+    #Calculate airdensity
     airdensity = tempk / (287.05 * airpressure * 100)
+    #Calculate Downforce
     downforce = 0.5 * WINGSPAN * CHORD * LIFTCOEFFICIENT * airdensity * airflow*100
+    #Add downforce with timestamp to database to be fetched
     downforce_add = {
     "Time:":time.ctime(),
-    "Downforce":str(round(downforce,3))
+    "Downforce":str(round(downforce,6))
     }
+    #Update the most recent downforce value
     downforce_update = {
-    "Downforce":str(round(downforce,3))
+    "Downforce":str(round(downforce,6))
     }
+    #Update the most recent airpressure value
     airpressure_update = {
-    "Pressure":str(round(airpressure,3))
+    "Pressure":str(round(airpressure,6))
     }
+    #Update the most recent temperature value
     temperature_update = {
-    "Temperature":str(round(temperature,3))
+    "Temperature":str(round(temperature,6))
     }
     print("Sending to cloud")
     #send the data to the cloud
@@ -70,42 +82,46 @@ def on_message(client, userdata, message):
     update_cloud(airpressure_update, "Pressure")
     update_cloud(temperature_update, "Temperature")
 
-
+#Function to connect to the MQTT broker
 def connecting():
 	try:
-		client.connect("146.169.222.168", port=1883)
+		client.connect("146.169.213.10", port=1883)
 	except:
 		print("Error connection unsuccessful")
+        #If connection fails try again
 		connecting()
 
 #initialize the client
 client = mqtt.Client()
+#Connect to the MQTT Broker
 connecting()
 print("Connection successful")
+#Client callback functions
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.subscribe("IC.embedded/Pantheon/#")
-
+#Define stream handler to detect firebase changes
 def stream_handler(message):
     if(message["data"]==1):
+        #Send message to broker to turn measuring on
         client.publish("IC.embedded/Pantheon/run","on")
         print("On")
     if(message["data"]==0):
+        #Send message to broker to turn measuring off
         client.publish("IC.embedded/Pantheon/run","off")
         print("Off")
 
+#Surveying changes in Firebase reading/value
 my_stream = db.child("Reading/Value").stream(stream_handler)
 
+#Sending data to cloud
 def send_to_cloud(data):
     print("Data: ", data)
     results = db.child("Downforces").push(data)
+
+#Updating data on cloud
 def update_cloud(data, topic):
     results = db.child(topic).update(data)
 
+#Infinite loop
 client.loop_forever()
-
-#endless loop
-#while True:
-#    client.loop()
-#    time.sleep(1)
--- mqtt_mosquitto.py
+++ mqtt_mosquitto.py
@@ -3,55 +3,78 @@ import time
 from i2c_reads import read_airflow_data
 from i2c_reads import read_pressure_and_temp_data
 
+#Variable to set whether pi is measuring or not
 run=True
 
+#When connected to MQTT:
 def on_connect(client, userdata, flags, rc):
+	# Subscribing in on_connect() means that if we lose the connection and
+	# reconnect then subscriptions will be renewed.
 	client.subscribe("IC.embedded/Pantheon/run")
 
+#When disconnected from MQTT try reconnecting
 def on_disconnect(client, userdata, rc):
 	print("disconnected with rtn code [%d]"% (rc))
 	connecting()
 
+#When a message is received from MQTT
 def on_message(client, userdata, message):
 	print(message.payload)
 	print("Received message")
+	#If message set global variable run
 	global run
+	#Turn measurement on
 	if(message.payload == b'on'):
 		print("MESSAGE ON")
 		run=True
+	#Turn measurement off
 	if(message.payload == b'off'):
 		print("MESSAGE OFF")
 		run=False
 
+#Set the client
 client = mqtt.Client()
 
+#Set encryption for MQTT connection
 client.tls_set(ca_certs="mosquitto.org.crt", certfile="client.crt",keyfile="client.key")
 
+#Function to connect to the MQTT broker
 def connecting():
 	try:
-		client.connect("146.169.222.168", port=8884)
+		client.connect("test.mosquitto.org", port=8884)
 	except:
+		#If connection failed try again:
 		print("Error connection unsuccessful")
-		#print(mqtt.error_string(RETURN_CODE))
 		connecting()
 
+#Connect to the MQTT Broker
 connecting()
 
+#Print on console
 print("Connection successful")
 
+#Publish first message onto MQTT Broker
 MSG_INFO = client.publish("IC.embedded/Pantheon/test","Connected to raspberry")
 print(mqtt.error_string(MSG_INFO.rc)) #MSG_INFO is result of publish()
 
+#Define callback functions
 client.on_message = on_message
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
 
+#Infinite loop
 while True:
+	#If run is set do the measurement
 	if (run==True):
+		#Function from i2c_ready
 		airflowsensordata=read_airflow_data()
+		#Function from i2c_ready
 		pressuresensordata, ctempdata = read_pressure_and_temp_data()
+		#Concatenate the data
 		concatData = str(ctempdata) + ',' + str(pressuresensordata) + ',' + str(airflowsensordata)
-		print(concatData)
+		#Publish concatenated data to the Broker
 		client.publish("IC.embedded/Pantheon/Measurement/concatData",concatData)
+	#Loop for the client
 	client.loop()
-	time.sleep(2)
+	#Sleep to save energy
+	time.sleep(1)
