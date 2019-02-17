d926b7269f442a7115a9ee62177cbf7636a28fa0
NiallRees <niallrwoodward@gmail.com>
2019-02-17 13:34:44 +0000

    Tidied up + references

--- i2c_reads.py
+++ i2c_reads.py
@@ -4,82 +4,61 @@ import smbus
 bus = smbus.SMBus(1)
 
 AIRFLOW_ADDRESS = 0x48 #address of the ADS1115
-PRESSURE_ADDRESS = 0x77 #address of the BMP280
 AIRFLOW_CONFIG_POINTER = 0x01
 AIRFLOW_CONV_POINTER = 0x00
-AIRFLOW_CONFIG_DATA = [0b10000010, 0b10000011] #all default except change to continuous conversion mode
+AIRFLOW_CONFIG_DATA = [0BMP280_data0000010, 0BMP280_data0000011] #all default except change to continuous conversion mode
 
-bus.write_i2c_block_data(AIRFLOW_ADDRESS, AIRFLOW_CONFIG_POINTER, AIRFLOW_CONFIG_DATA) #write the CONFIG_DATA
+bus.write_i2c_block_data(AIRFLOW_ADDRESS, AIRFLOW_CONFIG_POINTER, AIRFLOW_CONFIG_DATA) #write the CONFIG_DATA to the ADS1115
 
 
-
-#while 1:
-#    data = bus.read_i2c_block_data(DEVICE_ADDRESS, CONV_POINTER, 2)
-#    value = data[0] * 256 + data[1]
-#    if value > 32767:
-#	value -= 65535
-#    voltage = 0.000125*value + 0.62 # scaling factor to convert to actual voltage
-#    #ms = -(25/3111111111)*math.sqrt(24888888888000000*voltage-11462666687111111) - 783333325/3111111111
-#    print(voltage)
-#    time.sleep(0.05)
-
-def read_pressure_and_temp_data():
-    # BMP280 address, 0x77(118)
-    # Read data back from 0x88(136), 24 bytes
-    b1 = bus.read_i2c_block_data(0x77, 0x88, 24)
+def read_pressure_and_temp_data(): #Based on code from https://ae-bst.resource.bosch.com/media/_tech/media/datasheets/BST-BMP280-DS001.pdf and https://github.com/ControlEverythingCommunity/BMP280/blob/master/Python/BMP280.py
+    BMP280_data = bus.read_i2c_block_data(0x77, 0x88, 24) #Read data back from pressure sensor with i2c address 0x77, register 0x88(136), 24 bytes
 
     # Convert the data
     # Temp coefficents
-    dig_T1 = b1[1] * 256 + b1[0]
-    dig_T2 = b1[3] * 256 + b1[2]
+    dig_T1 = BMP280_data[1] * 256 + BMP280_data[0]
+    dig_T2 = BMP280_data[3] * 256 + BMP280_data[2]
     if dig_T2 > 32767 :
         dig_T2 -= 65536
-    dig_T3 = b1[5] * 256 + b1[4]
+    dig_T3 = BMP280_data[5] * 256 + BMP280_data[4]
     if dig_T3 > 32767 :
         dig_T3 -= 65536
 
     # Pressure coefficents
-    dig_P1 = b1[7] * 256 + b1[6]
-    dig_P2 = b1[9] * 256 + b1[8]
+    dig_P1 = BMP280_data[7] * 256 + BMP280_data[6]
+    dig_P2 = BMP280_data[9] * 256 + BMP280_data[8]
     if dig_P2 > 32767 :
         dig_P2 -= 65536
-    dig_P3 = b1[11] * 256 + b1[10]
+    dig_P3 = BMP280_data[11] * 256 + BMP280_data[10]
     if dig_P3 > 32767 :
         dig_P3 -= 65536
-    dig_P4 = b1[13] * 256 + b1[12]
+    dig_P4 = BMP280_data[13] * 256 + BMP280_data[12]
     if dig_P4 > 32767 :
         dig_P4 -= 65536
-    dig_P5 = b1[15] * 256 + b1[14]
+    dig_P5 = BMP280_data[15] * 256 + BMP280_data[14]
     if dig_P5 > 32767 :
         dig_P5 -= 65536
-    dig_P6 = b1[17] * 256 + b1[16]
+    dig_P6 = BMP280_data[17] * 256 + BMP280_data[16]
     if dig_P6 > 32767 :
         dig_P6 -= 65536
-    dig_P7 = b1[19] * 256 + b1[18]
+    dig_P7 = BMP280_data[19] * 256 + BMP280_data[18]
     if dig_P7 > 32767 :
         dig_P7 -= 65536
-    dig_P8 = b1[21] * 256 + b1[20]
+    dig_P8 = BMP280_data[21] * 256 + BMP280_data[20]
     if dig_P8 > 32767 :
         dig_P8 -= 65536
-    dig_P9 = b1[23] * 256 + b1[22]
+    dig_P9 = BMP280_data[23] * 256 + BMP280_data[22]
     if dig_P9 > 32767 :
         dig_P9 -= 65536
 
-    # BMP280 address, 0x77(118)
-    # Select Control measurement register, 0xF4(244)
-    #		0x27(39)	Pressure and Temperature Oversampling rate = 1
-    #					Normal mode
+    # Select Control measurement register, 0xF4(244), enter normal mode
     bus.write_byte_data(0x77, 0xF4, 0x27)
-    # BMP280 address, 0x77(118)
-    # Select Configuration register, 0xF5(245)
-    #		0xA0(00)	Stand_by time = 1000 ms
+    # Select Configuration register, 0xF5(245), set stand_by time = 1000 ms
     bus.write_byte_data(0x77, 0xF5, 0xA0)
 
     time.sleep(0.5)
 
-    # BMP280 address, 0x77(118)
     # Read data back from 0xF7(247), 8 bytes
-    # Pressure MSB, Pressure LSB, Pressure xLSB, Temperature MSB, Temperature LSB
     # Temperature xLSB, Humidity MSB, Humidity LSB
     data = bus.read_i2c_block_data(0x77, 0xF7, 8)
 
@@ -120,7 +99,5 @@ def read_airflow_data():
     airflow = (-0.3269/0.127) + (math.sqrt((0.254*airflowvalue) - 0.014) / 0.127)
     #airflowvoltage = 0.0053*((airflowvalue)^4) + 0.083*((airflowvalue)^3) + 0.3256*((airflowvalue)^2) + 0.0467*(airflowvalue) + 0.5 # scaling factor to convert to actual voltage
     #ms = -(25/3111111111)*math.sqrt(24888888888000000*voltage-11462666687111111) - 783333325/3111111111
-
-    pressure_data = bus.read_i2c_block_data(PRESSURE_ADDRESS, 0x88, 24)
 
     return airflow
